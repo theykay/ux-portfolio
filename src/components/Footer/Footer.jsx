7d5c7b37dd228a25e88449ccc8ed6ce432a93424
TheyKay <murraykyleb@gmail.com>
2022-04-10 11:31:17 -0600

    styling footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import './Footer.css';
+import './Footer.scss';
 
 const Footer = () => {
   return (
     <footer className="main-foot">
       <ul className="foot-nav">
         <li>
-          <Link to="/about">about</Link>
+          <Link className="foot-link" to="/about">about</Link>
         </li>
         <li>
-          <Link to="/work">work</Link>
+          <Link className="foot-link" to="/work">work</Link>
         </li>
-        <li>
-          <Link to="/connect">connect</Link>
-        </li>
+        {/* <li>
+          <Link clasName="foot-link" to="/connect">connect</Link>
+        </li> */}
       </ul>
       <h5>&#169;Kay Murray</h5>
     </footer>
