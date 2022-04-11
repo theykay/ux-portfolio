cab13ca62936932b36221a38c1315d49839267b4
TheyKay <murraykyleb@gmail.com>
2022-04-10 20:07:58 -0600

    styling home page, header for large screens

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,7 +4,12 @@ import './Home.scss';
 const Home = () => {
   return (
     <>
-      
+      <section className="home-section">
+        <h1>About Me</h1>
+      </section>
+      <section className="home-section">
+        <h1>Work</h1>
+      </section>
     </>
   );
 };
