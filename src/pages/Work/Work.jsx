2778861680768510ba9f851cc51dfffeb70e9f99
TheyKay <murraykyleb@gmail.com>
2022-04-13 20:52:14 -0600

    initialized work page

--- src/pages/Work/Work.jsx
+++ src/pages/Work/Work.jsx
@@ -4,7 +4,12 @@ import './Work.scss';
 const Work = () => {
   return (
     <div className="work-main">
-      
+      {/* carousel of case studies */}
+      <section className="case-studies"></section>
+      {/* banner link to exercises */}
+      <section className="exercises"></section>
+      {/* section for other personal design projects */}
+      <section className="other"></section>
     </div>
   );
 };
