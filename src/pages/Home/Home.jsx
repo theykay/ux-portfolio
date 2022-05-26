813285fda5601286a8466b5396fbd20fa07f1184
TheyKay <murraykyleb@gmail.com>
2022-05-25 20:46:11 -0600

    changed title, styled main div

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,14 +3,14 @@ import './Home.scss';
 
 const Home = () => {
   return (
-    <>
+    <div className="page-content">
       <section className="home-section">
         <h1>About Me</h1>
       </section>
       <section className="home-section">
         <h1>Work</h1>
       </section>
-    </>
+    </div>
   );
 };
 
