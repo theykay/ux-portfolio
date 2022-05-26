7ba2a54a9c7698b2031ce66e5ed0c406ff1a894f
TheyKay <murraykyleb@gmail.com>
2022-05-25 19:46:36 -0600

    removed links from footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,17 +5,17 @@ import './Footer.scss';
 const Footer = () => {
   return (
     <footer className="main-foot">
-      <ul className="foot-nav">
+      {/* <ul className="foot-nav">
         <li>
           <Link className="foot-link" to="/about">about</Link>
         </li>
         <li>
           <Link className="foot-link" to="/work">work</Link>
         </li>
-        {/* <li>
+        <li>
           <Link clasName="foot-link" to="/connect">connect</Link>
-        </li> */}
-      </ul>
+        </li>
+      </ul> */}
       <h5>&#169;Kay Murray</h5>
     </footer>
   );
