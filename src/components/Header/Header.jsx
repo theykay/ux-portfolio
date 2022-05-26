bf81fa4f97d32e8e41afb9a7320054c434bc08c6
TheyKay <murraykyleb@gmail.com>
2022-05-25 20:15:15 -0600

    added dropdown menu to work link, removed link reference in footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,14 +9,25 @@ const Header = () => {
       <nav>
         <Link className="nav-section" to="/">
           <img className="logo" src={logo} alt="logo" />
-          <h1>Kay Murray</h1>
+          <h1>theyKay</h1>
         </Link>
         <ul className="nav-ul">
+          <li className="nav-li  dd-trigger">
+            <Link className="nav-link" to="/work">work</Link>
+            <ul className="dd-nav">
+              <li className="dd-item">
+                <Link className="dd-link" to="/ux">ux case studies</Link>
+              </li>
+              <li className="dd-item">
+                <Link className="dd-link" to="/other">other</Link>
+              </li>
+            </ul>
+          </li>
           <li className="nav-li">
             <Link className="nav-link" to="/about">about</Link>
           </li>
           <li className="nav-li">
-            <Link className="nav-link" to="/work">work</Link>
+            <Link className="nav-link" to="/resume">resumé</Link>
           </li>
         </ul>
       </nav>
