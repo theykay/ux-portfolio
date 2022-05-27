df7a811eb426fc57df05324eb971946f0c266e9f
TheyKay <murraykyleb@gmail.com>
2022-05-26 22:26:34 -0600

    got collapsible lists working

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faCode,
+  faLaptopCode,
+  faPeopleGroup,
+  faPenRuler,
+  faObjectGroup
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faHtml5,
+  faCss3Alt,
+  faJsSquare,
+  faReact,
+  faSass,
+  faNodeJs
+} from '@fortawesome/free-brands-svg-icons';
 import './Home.scss';
 
 const Home = () => {
+  // need to remove classes when clicking outside e.target
+  const reveal = (e) => {
+    e.target.nextSibling.classList.toggle('unfolded');
+    e.target.classList.toggle('active-collapser');
+    let indicator = e.target.nextSibling.classList[1];
+    // collapse other lists
+    let lists = document.getElementsByClassName('about-skills-list');
+    for (let i = 0; i < lists.length; i++) {
+      if (i === parseInt(indicator)) {
+        continue;
+      }
+      lists[i].classList.remove('unfolded');
+    }
+    // unhighlight other collapsers
+    let togglers = document.getElementsByClassName('collapser');
+    for (let j = 0; j < togglers.length; j++) {
+      if (j === parseInt(indicator)) {
+        continue;
+      }
+      togglers[j].classList.remove('active-collapser');
+    }
+  }
+
   return (
     <div className="page-content">
       <h1 className="home-h1">ux + ui design</h1>
@@ -14,36 +53,45 @@ const Home = () => {
           </section>
           <section className="about-section">
             <h3>some of my skills</h3>
-            <h4>Design Systems</h4>
-            <ul>
-              <li>VSCode</li>
-              <li>Figma</li>
-              <li>InVision</li>
-              <li>Miro</li>
-              <li>Adobe XD</li>
-            </ul>
-            <h4>Creative Tools</h4>
-            <ul>
-              <li>Illustrator</li>
-              <li>InDesign</li>
-            </ul>
-            <h4>Languages</h4>
-            <ul>
-              <li>HTML5</li>
-              <li>CSS3</li>
-              <li>JavaScript ES6</li>
-              <li>React.js</li>
-              <li>SCSS</li>
-              <li>C# <span>learning</span></li>
-              <li>TypeScript <span>learning</span></li>
-            </ul>
-            <h4>project management</h4>
-            <ul>
-              <li>Discord</li>
-              <li>Slack</li>
-              <li>Trello</li>
-              <li>Teams</li>
-            </ul>
+            <div className="about-section-div">
+              <h4 className="collapser" onClick={reveal}><FontAwesomeIcon className="section-icon" icon={faObjectGroup} />Design Systems</h4>
+              <ul className="about-skills-list 0">
+                <li>VSCode</li>
+                <li>Figma</li>
+                <li>InVision</li>
+                <li>Miro</li>
+                <li>Adobe XD</li>
+              </ul>
+            </div>
+            <div className="about-section-div">
+              <h4 className="collapser" onClick={reveal}><FontAwesomeIcon className="section-icon" icon={faPenRuler} />Creative Tools</h4>
+              <ul className="about-skills-list 1">
+                <li>Illustrator</li>
+                <li>InDesign</li>
+              </ul>
+            </div>
+            <div className="about-section-div">
+              <h4 className="collapser" onClick={reveal}><FontAwesomeIcon className="section-icon" icon={faCode} />Languages</h4>
+              <ul className="about-skills-list 2">
+                <li><FontAwesomeIcon className="list-icon" icon={faHtml5} />HTML5</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faCss3Alt} />CSS3</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faJsSquare} />JavaScript ES6</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faReact} />React.js</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faSass} />SCSS</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faNodeJs} />Node.js</li>
+                <li><FontAwesomeIcon className="list-icon" icon={faLaptopCode} />C# <span>learning</span></li>
+                <li><FontAwesomeIcon className="list-icon" icon={faLaptopCode} />TypeScript <span>learning</span></li>
+              </ul>
+            </div>
+            <div className="about-section-div">
+              <h4 className="collapser" onClick={reveal}><FontAwesomeIcon className="section-icon" icon={faPeopleGroup} />Project Management</h4>
+              <ul className="about-skills-list 3">
+                <li>Discord</li>
+                <li>Slack</li>
+                <li>Trello</li>
+                <li>Teams</li>
+              </ul>
+            </div>
           </section>
         </article>
       </section>
