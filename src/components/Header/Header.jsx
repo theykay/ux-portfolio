6edc69321fecf55632a2b840112046598d4899c6
TheyKay <murraykyleb@gmail.com>
2022-06-09 20:08:18 -0600

    made header sticky

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,19 +12,20 @@ const Header = () => {
           <h1>theyKay</h1>
         </Link>
         <ul className="nav-ul">
+          <li className="nav-li">
+            <Link className="nav-link" to="/about">about</Link>
+          </li>
           <li className="nav-li  dd-trigger">
             <Link className="nav-link" to="/work">work</Link>
-            <ul className="dd-nav">
+            {/* dropdown menu */}
+            {/* <ul className="dd-nav">
               <li className="dd-item">
                 <Link className="dd-link" to="/ux">ux case studies</Link>
               </li>
               <li className="dd-item">
                 <Link className="dd-link" to="/other">other</Link>
               </li>
-            </ul>
-          </li>
-          <li className="nav-li">
-            <Link className="nav-link" to="/about">about</Link>
+            </ul> */}
           </li>
           <li className="nav-li">
             <Link className="nav-link" to="/resume">resumé</Link>
