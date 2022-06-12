2ada91a8221428963b0f081f2e65f9991623be11
TheyKay <murraykyleb@gmail.com>
2022-06-12 12:30:43 -0600

    added routes for projects

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import { Header, Footer } from "./components/index.js";
-import { Home, About, Work } from "./sections/index.js";
+import { Home, About, Work, Project } from "./sections/index.js";
 
 const App = () => {
   return (
@@ -21,7 +21,11 @@ const App = () => {
             }
           />
           {/* routes for different projects */}
-          {/* <Route path="wandr" element={<About />} /> */}
+          <Route path="wandr" element={<Project name="wandr" />} />
+          <Route path="childcare" element={<Project name="childcare" />} />
+          <Route path="usda" element={<Project name="usda" />} />
+          <Route path="charity" element={<Project name="charity" />} />
+          <Route path="coffee" element={<Project name="coffee" />} />
         </Routes>
         <Footer />
       </Router>
--- src/sections/index.js
+++ src/sections/index.js
@@ -1,3 +1,4 @@
 export { default as About } from './About/About';
 export { default as Home } from './Home/Home';
 export { default as Work } from './Work/Work';
+export { default as Project } from './Project/Project';
